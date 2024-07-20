ac05ce4fe4c131e05b6216cd23157a247cfeb649
Zavier Miller <zavierjmiller@gmail.com>
2024-07-20 14:18:17 -0500

    fix bin

--- src/main.ts
+++ src/main.ts
@@ -110,7 +110,7 @@ const args = program.args;
 const projectDir = path.resolve(args[args.length - 1]);
 const entryPoint = path.resolve(program.opts().entryPoint);
 
-const graph = DependencyGraph.load('dependency-graph.json');
+const graph = new DependencyGraph();
 
 if (args[0] === 'report') {
   const unusedFiles = findUnusedFiles(graph, entryPoint, projectDir);
